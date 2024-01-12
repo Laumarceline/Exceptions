d4db0a0335df243559dea6952878a4199ec38d1c
Laumarceline <laura.rozo2308@gmail.com>
2024-01-12 20:24:15 -0300

    Agrego línea adicional

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         CuentaBancaria c = new CuentaBancaria(1000);
         c.extraer(2000);
+        c.extraer(3000);
+
     }
 }
